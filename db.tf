c77f05eda2e42014b6743a808fa151f8911b0e1b
Igor Grinkin <igorgrin@gmail.com>
2017-07-26 21:19:51 +0000

    adding encrypted password

--- db.tf
+++ db.tf
@@ -1,3 +1,10 @@
+data "aws_kms_secret" "db" {
+  secret {
+    name    = "master_password"
+    payload = "AQICAHgmvmEfxy24Iq+NEbuUYjNKO45BP0qDi3q1xmdaE7+C4AFOuFmM4A7H9mlxpODrfqBHAAAAbTBrBgkqhkiG9w0BBwagXjBcAgEAMFcGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMxoJjKlun5k9EV2V8AgEQgCphv2dyhg6YitnCwBfrku1RHyl6FOZ775z7VcbQq5Q7QqnzO40STDC0rTg="
+  }
+}
+
 resource "aws_db_subnet_group" "krdb_group" {
   name       = "krdb_group"
   subnet_ids = ["${aws_subnet.default.id}","${aws_subnet.b.id}"]
@@ -15,7 +22,7 @@ resource "aws_db_instance" "krdb1" {
   instance_class       = "db.t2.micro"
   name                 = "krdb1"
   username             = "kradmin"
-  password             = "blahblah"
+  password             = "${data.aws_kms_secret.db.master_password}"
   db_subnet_group_name = "krdb_group"
   vpc_security_group_ids = ["${aws_security_group.db.id}"]
 }
